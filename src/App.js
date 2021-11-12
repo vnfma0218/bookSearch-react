73ee02eca87201b86af9c4d03f94e861d2719a21
pooreum <vnfma0218@naver.com>
2021-11-13 01:43:54 +0900

    start booksearch prj

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+import SearchBook from './components/SearchBook';
 function App() {
+  const [bookList, setBookList] = useState([]);
+  console.log(bookList);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SearchBook setBookList={(data) => setBookList(data)} />
     </div>
   );
 }
---
+++ src/components/SearchBook.js
@@ -0,0 +1,44 @@
+import { Button, Input } from '@mui/material';
+import axios from 'axios';
+import React, { useState } from 'react';
+
+export default function SearchBook(props) {
+  const [searchTerm, setSearchTerm] = useState('');
+  const searchSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    const response = await axios.get(
+      'https://dapi.kakao.com/v3/search/book?target=title',
+      {
+        method: 'GET',
+        params: { query: searchTerm },
+        headers: {
+          Authorization: `KakaoAK ${process.env.REACT_APP_KEY}`,
+        },
+      }
+    );
+    const data = await response.data.documents;
+    console.log(response);
+    props.setBookList(data);
+    setSearchTerm('');
+  };
+  return (
+    <section className='search__container'>
+      <h1>책검색하기</h1>
+      <form onSubmit={searchSubmitHandler}>
+        <Input
+          id='standard-search'
+          label='Search field'
+          type='search'
+          variant='standard'
+          placeholder='Search your books'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <Button variant='contained' onClick={searchSubmitHandler}>
+          검색
+        </Button>
+      </form>
+    </section>
+  );
+}
